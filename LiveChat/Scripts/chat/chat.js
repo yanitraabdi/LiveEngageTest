6a405558e77e5c029723e9cdd8faea621268a783
Muhammad Habib <habieb.ilkomp@gmail.com>
2017-09-18 17:03:29 +0700

    chat area

--- LiveChat/Scripts/chat/chat.js
+++ LiveChat/Scripts/chat/chat.js
@@ -1,11 +1,12 @@
 ﻿var globalToken = "";
 var agentSession = "";
 var accountNumber = "";
+var currentChatId = "";
 
 $(document).ready(function () {
     if (globalToken == "" && agentSession == "") {
-    $("#panelLogin").show();
-    $("#panelTable").hide();
+        $("#panelLogin").show();
+        $("#panelTable").hide();
     }
     else {
         $("#panelLogin").hide();
@@ -74,6 +75,7 @@ var chat = {
                 chat.incomingchatrequest(accountnumber, agentSession);
 
                 chat.chatsessions(accountnumber);
+                chat.getchatevent();
             },
             error: function () {
                 console.log("agentsession Error (API)")
@@ -98,9 +100,11 @@ var chat = {
 
                 $("#divChatList").html("");
 
-                $.each(parsedData.chats.chat, function(i, obj) {
+                $.each(parsedData.chats.chat, function (i, obj) {
                     divChatList.innerHTML += '<li class="left clearfix" id="' + obj.info.chatSessionKey + '"><span class="chat-img pull-left"><img src="http://www.joomilak.com/media/com_easydiscuss/images/default_avatar.png" alt="User Avatar" class="img-circle"></span><div class="chat-body clearfix" ><div class="header_sec"><strong class="primary-font">' + obj.info.visitorName + '</strong> <strong class="pull-right">' + moment(obj.info.lastUpdate).format("HH:mm") + '</strong></div><div class="contact_sec">' + obj.info.visitorId + '<span class="badge pull-right">3</span></div>';
+                    $("#" + obj.info.chatSessionKey).click(function () { currentChatId = obj.info.chatSessionKey; })
                 });
+
             },
             error: function () {
                 console.log("Login Error (API)")
@@ -199,33 +203,43 @@ var chat = {
             }
         });
     },
-    getchatevent: function (chatSessionKey) {
-        $.ajax({
-            url: "/LiveChat/api/v1/chats",
-            data: {
-                url: "https://sy.agentvep.liveperson.net/api/account/" + accountnumber + "/agentSession/" + agentSession + "/chatSessions?v=1&NC=true",
-                method: "GET",
-                body: "",
-                token: globalToken
-            },
-            success: function (data) {
-                var parsedData = JSON.parse(data);
+    getchatevent: function () {
+        if (currentChatId != "") {
+            $.ajax({
+                url: "/LiveChat/api/v1/chats",
+                data: {
+                    url: "https://sy.agentvep.liveperson.net/api/account/" + accountNumber + "/agentSession/" + agentSession + "/chat/" + currentChatId + "/events?v=1&NC=true",
+                    method: "GET",
+                    body: "",
+                    token: globalToken
+                },
+                success: function (data) {
+                    var parsedData = JSON.parse(data);
 
-                console.log(parsedData);
+                    console.log(parsedData);
 
-                var arrChatSession = [];
+                    $("#chat-events").html('');
 
-                $.each(parsedData.chatSessions.chatSession, function (i, obj) {
-                    arrChatSession.push(obj.chatSessionKey);
-                })
+                    $.each(parsedData.events.event, function (i, obj) {
+                        //console.log('<li class="left clearfix ' + obj.source == "agent" ? "admin_chat" : "" + '"><span class="chat-img1 ' + obj.source == "agent" ? "pull-right" : "pull-left" + '"><img src="' + obj.source == "agent" ? "https://lh6.googleusercontent.com/-y-MY2satK-E/AAAAAAAAAAI/AAAAAAAAAJU/ER_hFddBheQ/photo.jpg" : "http://www.joomilak.com/media/com_easydiscuss/images/default_avatar.png" + '" alt="User Avatar" class="img-circle"></span><div class="chat-body1 clearfix"><p>' + obj.text + '</p><div class="chat_time ' + obj.source == "agent" ? "pull-left" : "pull-right" + '">' + moment(obj.time).format("HH:mm") + '</div></div></li>');
+                        if (obj["@type"] == "line") {
+                            if (obj.source == "agent")
+                                $("#chat-events").append('<li class="left clearfix admin_chat"><span class="chat-img1 pull-right"><img src="https://lh6.googleusercontent.com/-y-MY2satK-E/AAAAAAAAAAI/AAAAAAAAAJU/ER_hFddBheQ/photo.jpg" alt="User Avatar" class="img-circle"></span><div class="chat-body1 clearfix"><p>' + obj.text + '</p><div class="chat_time pull-left">' + moment(obj.time).format("HH:mm") + '</div></div></li>');
+                            else
+                                $("#chat-events").append('<li class="left clearfix"><span class="chat-img1 pull-left"><img src="http://www.joomilak.com/media/com_easydiscuss/images/default_avatar.png" alt="User Avatar" class="img-circle"></span><div class="chat-body1 clearfix"><p>' + obj.text + '</p><div class="chat_time pull-right">' + moment(obj.time).format("HH:mm") + '</div></div></li>');
+                        }
+                    });
 
-                var joinArray = arrChatSession.join(",")
-
-                chat.chatlist(accountnumber, joinArray);
-            },
-            error: function () {
-                console.log("Login Error (API)")
-            }
-        });
+                    setTimeout(function () { chat.getchatevent(); }, 1000);
+                },
+                error: function () {
+                    console.log("Get Chat Event Error (API)");
+                    setTimeout(function () { chat.getchatevent(); }, 1000);
+                }
+            });
+        }
+        else {
+            setTimeout(function () { chat.getchatevent(); }, 1000);
+        }
     }
 }
